08a9b27d2443e3571f556b961384955baf841fcf
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-09-06 17:28:54 +0530

    Updates

--- precache-manifest.9421757739951ccee7ed19b45ddaa516.js
+++ precache-manifest.9421757739951ccee7ed19b45ddaa516.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "36047a9bcfc787ee3f41cb643ecb1c94",
+    "revision": "6418a790570e438fb73071dac1e2f696",
     "url": "/face-detector/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/face-detector/static/css/2.3c03f26f.chunk.css"
   },
   {
-    "revision": "5b35f8b42bff6f30643b",
+    "revision": "b0fa31fc9f67c1b218e1",
     "url": "/face-detector/static/css/main.b3f487ad.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/face-detector/static/js/2.bfdf827c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5b35f8b42bff6f30643b",
-    "url": "/face-detector/static/js/main.1088738d.chunk.js"
+    "revision": "b0fa31fc9f67c1b218e1",
+    "url": "/face-detector/static/js/main.e312fdb8.chunk.js"
   },
   {
     "revision": "15b01d090c9ea2879c69",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/face-detector/precache-manifest.1037b9ead71de06ed08e22adf07f3e0d.js"
+  "/face-detector/precache-manifest.9421757739951ccee7ed19b45ddaa516.js"
 );
 
 self.addEventListener('message', (event) => {
