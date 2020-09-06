2c105b625aa320220233501a5acf2e035eea07f4
Dinesh Sadhwani <dineshsadhwani4@gmail.com>
2020-09-06 17:05:21 +0530

    Updates

---
+++ precache-manifest.1037b9ead71de06ed08e22adf07f3e0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36047a9bcfc787ee3f41cb643ecb1c94",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "92ae2d3f7f2dcdbdb1b7",
+    "url": "/face-detector/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5b35f8b42bff6f30643b",
+    "url": "/face-detector/static/css/main.b3f487ad.chunk.css"
+  },
+  {
+    "revision": "92ae2d3f7f2dcdbdb1b7",
+    "url": "/face-detector/static/js/2.bfdf827c.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-detector/static/js/2.bfdf827c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b35f8b42bff6f30643b",
+    "url": "/face-detector/static/js/main.1088738d.chunk.js"
+  },
+  {
+    "revision": "15b01d090c9ea2879c69",
+    "url": "/face-detector/static/js/runtime-main.478dc112.js"
+  }
+]);
